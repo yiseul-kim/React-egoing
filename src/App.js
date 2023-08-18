6b5b8e6f23f004221ca515d6f4a8ae4ec1c680c1
yiseul_notebook <kimdew402@gmail.com>
2023-08-18 12:43:52 +0900

    9-10강

--- src/App.js
+++ src/App.js
@@ -20,6 +20,43 @@ function Nav(props) {
   </ol>
 </nav>
 }
+function Create(props){
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="title"/></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>
+}
+function Update(props){
+  const [title, setTitle] = useState(props.title); 
+  const [body, setBody] = useState(props.body);
+  return <article>
+  <h2>Update</h2>
+  <form onSubmit={event=>{
+    event.preventDefault();
+    const title = event.target.title.value;
+    const body = event.target.body.value;
+    props.onUpdate(title, body);
+  }}>
+    <p><input type="text" name="title" placeholder="title" value={title} onChange={(event=>{
+      console.log(event.target.value);
+      setTitle(event.target.value);
+    })}/></p>
+    <p><textarea name="body" placeholder="body" value={body} onChange={(event=>{
+      setBody(event.target.value);
+    })}></textarea></p>
+    <p><input type="submit" value="Update"></input></p>
+  </form>
+</article>
+}
 function Article(props) {
   return <article>
   <h2>{props.title}</h2>
@@ -38,24 +75,73 @@ function Header(props){
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
-  ]
+  ]);
   let content = null;
+  let contextControl = null;
   if(mode ==='WELCOME'){
     content = <Article title="Welcome" body="Hello, web"></Article>
   } else if (mode === 'READ'){
     let title, body = null;
     for(let i=0; i<topics.length; i++){
       if(topics[i].id === id){
-        console.log(topics[i].id, id);
         title = topics[i].title;
         body = topics[i].body;
       }
     }
     content = <Article title={title} body={body}></Article>
+    contextControl = <>
+      <li><a href={'/update/'+id} onClick={event=>{
+      event.preventDefault();
+      setMode('UPDATE');
+    }}>Update</a></li>
+    <li><input type="button" value="Delete" onClick={()=>{
+      const newTopics = []
+      for(let i=0; i<topics.length; i++) {
+        if(topics[i].id !== id){
+          newTopics.push(topics[i]);
+        }
+      }
+      setTopics(newTopics);
+      setMode('WELCOME');
+    }}/>
+    </li>
+    </>
+  } else if(mode === 'CREATE'){
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId, title:_title, body:_body}
+      const newTopics = [...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>
+  } else if(mode === 'UPDATE' ){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      if(topics[i].id === id ){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Update title={title} body={body} onUpdate={(title, body)=>{
+      console.log(title.body);
+      const newTopics = [...topics]
+      const updatedTopic = {id:id, title:title, body:body}
+      for(let i=0; i<newTopics.length; i++){
+        if(newTopics[i].id === id){
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+      setMode('READ');
+    }}></Update>
   }
   return (
     <div>
@@ -67,6 +153,13 @@ function App() {
         setId(_id);
       }}></Nav>
     {content}
+    <ul>
+     <li> <a href="/create" onClick={event=>{
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a></li>
+      {contextControl}
+    </ul>
     </div>
   );
 }
